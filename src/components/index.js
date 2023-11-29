49bb42699a1672029d9412fa0040f9f6a0ecbee7
GhitaB <ghitabizau27@gmail.com>
2023-11-29 15:31:08 +0200

    Refs #149679 - Move code from volto-freshwater-policy.

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export FavButton from './theme/FavBoard/FavButton';
+export FavBoardView from './theme/FavBoard/FavBoardView';
+export FavItemToolbar from './theme/FavBoard/FavItemToolbar';
+export FavBoardComments from './theme/FavBoard/FavBoardComments';
+export FavBoardListingView from './theme/FavBoard/FavBoardListingView';
